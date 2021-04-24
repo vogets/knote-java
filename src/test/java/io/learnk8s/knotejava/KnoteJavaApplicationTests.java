7a7e978f6bc0e9481c69cc523f3a218ee3733af1
vogets <archsameerudemy@gmail.com>
2021-04-24 15:07:55 +0530

    Initial Commit

---
+++ src/test/java/io/learnk8s/knotejava/KnoteJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.learnk8s.knotejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnoteJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
